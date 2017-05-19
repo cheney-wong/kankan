4d319af98e454b11f8150ea06ee9a68a39b85b5e
abdias <abdias520@163.com>
2017-05-19 23:09:39 +0800

    轮换版

--- js/js.js
+++ js/js.js
@@ -1,4 +1,6 @@
 $(function(){
+	var timer = '';//定时器
+	var i = 0;
 	// input表单获得焦点
 	$('.search_text').focus(function(){
 		$(this).attr('value','');
@@ -8,5 +10,18 @@ $(function(){
 	$('.search_text').blur(function(){
 		$(this).removeClass('blue').addClass('search_text');
 	})
+
+	//轮换版
+	function move(){
+		if(i>11) {
+			i = 0;
+		}
+		var backImg = $('.smal_pic li').eq(i).find('img').attr('backImg');
+		var backColor = $('.smal_pic li').eq(i).find('img').attr('backColor');
+		var background = 'url(' + backImg + ') ' + 'no-repeat center ' + backColor;
+		$('#focus_background').css({background : background});
+		$('.smal_pic li').eq(i).addClass('hover').siblings().removeClass('hover');
+		i++;
+	}
 
 })
